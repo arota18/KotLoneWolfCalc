da2b0e30fa4fa6e85bfe5236bfb77317b8366621
arota18 <104698199+arota18@users.noreply.github.com>
2022-05-16 10:54:50 +0200

    added superclass TwoCounterActivity

--- app/src/main/java/it/arota18/kotlonewolfcalc/MainActivity.kt
+++ app/src/main/java/it/arota18/kotlonewolfcalc/MainActivity.kt
@@ -3,7 +3,7 @@ package it.arota18.kotlonewolfcalc
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: TwoCounterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/it/arota18/kotlonewolfcalc/TwoCounterActivity.kt
@@ -0,0 +1,38 @@
+package it.arota18.kotlonewolfcalc
+
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+open class TwoCounterActivity : AppCompatActivity() {
+    /**
+     * view ref.
+     */
+    protected var etEndurance: EditText? = null
+
+    /**
+     * view ref.
+     */
+    protected var etCombat: EditText? = null
+
+    /**
+     * protected method used to increase/decrease endurance and combat counter
+     *
+     * @param v    View
+     * @param mode int value that represent which button has been pressed
+     */
+    protected fun editTxtValue(v: View?, mode: Int) {
+        var `val` = -1
+        val et: EditText? = if (mode < 3) etEndurance else etCombat
+        try {
+            `val` = et!!.text.toString().toInt()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        if (mode % 2 == 0)
+            if (`val` == -1) `val` = 1
+            else `val`++
+        else if (`val` > 0) `val`--
+        if (`val` != -1) et!!.setText(`val`.toString())
+    }
+}
